3e531e82ef5475a33a65ed3115714c7616c1b92e
Rexben001 <rexben.rb@gmail.com>
2019-10-14 15:08:10 +0100

    complete stack and queue

--- queue_and_stack/doubly_linked_list.py
+++ queue_and_stack/doubly_linked_list.py
@@ -100,9 +100,12 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_tail(self):
-        value = self.tail.value
-        self.delete(self.tail)
-        return value
+        if self.tail == None:
+            return None
+        else:
+            value = self.tail.value
+            self.delete(self.tail)
+            return value
 
     """Removes the input node from its current spot in the
     List and inserts it as the new head node of the List."""
